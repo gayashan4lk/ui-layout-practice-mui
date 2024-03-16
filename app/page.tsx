a6df119bacc5e0bd0dea3b30cdd1a22bdace8702
gayashan4lk <gayashan4lk@gmail.com>
2024-03-16 17:05:11 +0530

    extract TourCard

--- app/page.tsx
+++ app/page.tsx
@@ -8,16 +8,32 @@ export default async function Home() {
     <Container maxWidth="xl">
       <Grid container spacing={2}>
         {cities[1].tours.map((tour: any) => (
-          <Grid key={tour.id} xs={3}>
-            <Paper elevation={3} sx={{ padding: 2, backgroundColor: 'white' }}>
-              <Typography variant="subtitle2">{tour.name}</Typography>
-              <Typography variant="body2">$ {tour.price}</Typography>
-              <Typography variant="body2">url</Typography>
-              <Typography variant="body2">date</Typography>
-            </Paper>
-          </Grid>
+          <TourCard tour={tour} />
         ))}
       </Grid>
     </Container>
+  )
+}
+
+type TourCardProps = {
+  tour: any
+}
+
+function TourCard({ tour }: TourCardProps) {
+  return (
+    <Grid key={tour.id} xs={3}>
+      <Paper
+        elevation={3}
+        sx={{
+          padding: 2,
+          backgroundColor: 'white',
+        }}
+      >
+        <Typography variant="subtitle2">{tour.name}</Typography>
+        <Typography variant="body2">$ {tour.price}</Typography>
+        <Typography variant="body2">url</Typography>
+        <Typography variant="body2">date</Typography>
+      </Paper>
+    </Grid>
   )
 }
