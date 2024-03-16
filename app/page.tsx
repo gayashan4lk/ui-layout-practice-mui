e700f39bfabf310443d81a074cb0f547e094d6e2
gayashan4lk <gayashan4lk@gmail.com>
2024-03-16 16:59:38 +0530

    create a grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,23 @@
-export default function Home() {
-  return <div>Hi</div>
+import { Container, Paper, Typography } from '@mui/material'
+import Grid from '@mui/material/Unstable_Grid2'
+
+export default async function Home() {
+  const cities = await (await fetch('http://localhost:3000/api/cities')).json()
+
+  return (
+    <Container maxWidth="xl">
+      <Grid container spacing={2}>
+        {cities[1].tours.map((tour: any) => (
+          <Grid key={tour.id} xs={3}>
+            <Paper elevation={3} sx={{ padding: 2, backgroundColor: 'white' }}>
+              <Typography variant="subtitle2">{tour.name}</Typography>
+              <Typography variant="body2">$ {tour.price}</Typography>
+              <Typography variant="body2">url</Typography>
+              <Typography variant="body2">date</Typography>
+            </Paper>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  )
 }
